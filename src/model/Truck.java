2bdac1f1740d7ad42b40cf1f90fdbc1223597446
Manuel Balanta <mbalantaq@gmail.com>
2019-05-25 18:29:41 -0500

    Corregido el movimiento del carro

--- src/model/Truck.java
+++ src/model/Truck.java
@@ -10,23 +10,45 @@ public class Truck  extends Vehicule{
 	
 	private Truck next;
 	private Truck prev;
-
+	
+	/**
+	 * 
+	 * @param color
+	 * @param width
+	 * @param height
+	 */
 	public Truck(String color, double width, double height) {
 		super(color, width, height);
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Truck getNext() {
 		return next;
 	}
 
+	/**
+	 * 
+	 * @param next
+	 */
 	public void setNext(Truck next) {
 		this.next = next;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Truck getPrev() {
 		return prev;
 	}
 
+	/**
+	 * 
+	 * @param prev
+	 */
 	public void setPrev(Truck prev) {
 		this.prev = prev;
 	}
--- src/ui/RaceController.java
+++ src/ui/RaceController.java
@@ -3,16 +3,18 @@ package ui;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-//import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Rectangle;
 //import model.Game;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+
 
 /**
- * 
+ * Clase controladora del archvio (race) fxml
  * @author Manuel Balanta
  *
  */
@@ -20,20 +22,56 @@ import javafx.scene.paint.Color;
 public class RaceController {
 	
 	//private Game game;
-	
 	private Rectangle car; 
-	private Rectangle motor;
-	
+	private Rectangle wheelL1;
+	private Rectangle wheelL2;
+	private Rectangle wheelR1;
+	private Rectangle wheelR2;
 	@FXML
     private Pane lane;
-	
 	@FXML
 	private Button btnPlay;
+	@FXML
+	private Label lblPoints;
+	@FXML
+	private Label lblLifes;
+	@FXML
+    private Label lblControl;
+	@FXML
+	private Button btnUp;
+	@FXML
+	private Button btnDown;
+	@FXML
+	private Button btnLeft;
+	@FXML
+    private Button btnRight;
+	protected Tooltip ttU;
+	protected Tooltip ttD;
+	protected Tooltip ttL;
+	protected Tooltip ttR;
 	
+	/**
+	 * Método que inicializa la clase
+	 */
 	public void initialize() {
 		//game = new Game();
 		car = new Rectangle(95, 140);
-		motor = new Rectangle(81, 57);
+		wheelL1 = new Rectangle(12, 25);
+		wheelL2 = new Rectangle(12, 25);
+		wheelR1 = new Rectangle(12, 25);
+		wheelR2 = new Rectangle(12, 25);
+		ttU = new Tooltip();
+		ttD = new Tooltip();
+		ttR = new Tooltip();
+		ttL = new Tooltip();
+		ttU.setText("Usalo para mover el\ncarro hacia arriba");
+		ttD.setText("Usalo para mover el\ncarro hacia abajo");
+		ttR.setText("Usalo para mover el\ncarro hacia la derecha");
+		ttL.setText("Usalo para mover el\ncarro hacia la izquierda");
+		btnUp.setTooltip(ttU);
+		btnDown.setTooltip(ttD);
+		btnLeft.setTooltip(ttL);
+		btnRight.setTooltip(ttR);
 	}
 	
 	/**
@@ -44,42 +82,106 @@ public class RaceController {
 	public void play(ActionEvent event) {
 		Color randomColor = new Color(Math.random(),Math.random(),Math.random(),1);
 		car.setFill(randomColor);
-		motor.setFill(randomColor);
+		wheelL1.setFill(Color.BLACK);
+		wheelL2.setFill(Color.BLACK);
+		wheelR1.setFill(Color.BLACK);
+		wheelR2.setFill(Color.BLACK);
 		car.setLayoutX(253);
 		car.setLayoutY(520);
-		motor.setLayoutX(260);
-		motor.setLayoutY(463);
+		wheelL1.setLayoutX(241);
+		wheelL1.setLayoutY(529);
+		wheelL2.setLayoutX(242);
+		wheelL2.setLayoutY(629);
+		wheelR1.setLayoutX(348);
+		wheelR1.setLayoutY(529);
+		wheelR2.setLayoutX(348);
+		wheelR2.setLayoutY(629);
 		lane.getChildren().add(car);
-		lane.getChildren().add(motor);
+		lane.getChildren().add(wheelL1);
+		lane.getChildren().add(wheelL2);
+		lane.getChildren().add(wheelR1);
+		lane.getChildren().add(wheelR2);
 		btnPlay.setVisible(false);
-		car.setOnMouseClicked(new EventHandler<Event>() {
+		btnUp.setVisible(true);
+		btnDown.setVisible(true);
+		btnLeft.setVisible(true);
+		btnRight.setVisible(true);
+		lblControl.setVisible(true);
+		btnUp.setOnMouseClicked(new EventHandler<Event>() {
 			@Override
 			public void handle(Event event) {
-				car.setLayoutX(car.getLayoutX()+25);
-				motor.setLayoutX(motor.getLayoutX()+25);
+				if (car.getLayoutY()<=10) {
+					car.setLayoutY(car.getLayoutY()+15);
+					wheelL1.setLayoutY(wheelL1.getLayoutY()+15);
+					wheelL2.setLayoutY(wheelL2.getLayoutY()+15);
+					wheelR1.setLayoutY(wheelR1.getLayoutY()+15);
+					wheelR2.setLayoutY(wheelR2.getLayoutY()+15);
+				} else {
+					car.setLayoutY(car.getLayoutY()-25);
+					wheelL1.setLayoutY(wheelL1.getLayoutY()-25);
+					wheelL2.setLayoutY(wheelL2.getLayoutY()-25);
+					wheelR1.setLayoutY(wheelR1.getLayoutY()-25);
+					wheelR2.setLayoutY(wheelR2.getLayoutY()-25);
+				}
 				
 			}
 		});
-		motor.setOnMouseClicked(new EventHandler<Event>() {
+		btnDown.setOnMouseClicked(new EventHandler<Event>() {
 			@Override
 			public void handle(Event event) {
-				car.setLayoutX(car.getLayoutX()-25);
-				motor.setLayoutX(motor.getLayoutX()-25);
+				if (car.getLayoutY()>=525) {
+					car.setLayoutY(car.getLayoutY()-15);
+					wheelL1.setLayoutY(wheelL1.getLayoutY()-15);
+					wheelL2.setLayoutY(wheelL2.getLayoutY()-15);
+					wheelR1.setLayoutY(wheelR1.getLayoutY()-15);
+					wheelR2.setLayoutY(wheelR2.getLayoutY()-15);
+				} else {
+					car.setLayoutY(car.getLayoutY()+25);
+					wheelL1.setLayoutY(wheelL1.getLayoutY()+25);
+					wheelL2.setLayoutY(wheelL2.getLayoutY()+25);
+					wheelR1.setLayoutY(wheelR1.getLayoutY()+25);
+					wheelR2.setLayoutY(wheelR2.getLayoutY()+25);
+				}
+				
+			}
+		});
+		btnRight.setOnMouseClicked(new EventHandler<Event>() {
+			@Override
+			public void handle(Event event) {
+				if (car.getLayoutX()>=480) {
+					car.setLayoutX(car.getLayoutX()-15);
+					wheelL1.setLayoutX(wheelL1.getLayoutX()-15);
+					wheelL2.setLayoutX(wheelL2.getLayoutX()-15);
+					wheelR1.setLayoutX(wheelR1.getLayoutX()-15);
+					wheelR2.setLayoutX(wheelR2.getLayoutX()-15);
+				} else {
+					car.setLayoutX(car.getLayoutX()+25);
+					wheelL1.setLayoutX(wheelL1.getLayoutX()+25);
+					wheelL2.setLayoutX(wheelL2.getLayoutX()+25);
+					wheelR1.setLayoutX(wheelR1.getLayoutX()+25);
+					wheelR2.setLayoutX(wheelR2.getLayoutX()+25);
+				}
+			}
+		});
+		btnLeft.setOnMouseClicked(new EventHandler<Event>() {
+			@Override
+			public void handle(Event event) {
+				if (car.getLayoutX()<=20) {
+					car.setLayoutX(car.getLayoutX()+15);
+					wheelL1.setLayoutX(wheelL1.getLayoutX()+15);
+					wheelL2.setLayoutX(wheelL2.getLayoutX()+15);
+					wheelR1.setLayoutX(wheelR1.getLayoutX()+15);
+					wheelR2.setLayoutX(wheelR2.getLayoutX()+15);
+				} else {
+					car.setLayoutX(car.getLayoutX()-25);
+					wheelL1.setLayoutX(wheelL1.getLayoutX()-25);
+					wheelL2.setLayoutX(wheelL2.getLayoutX()-25);
+					wheelR1.setLayoutX(wheelR1.getLayoutX()-25);
+					wheelR2.setLayoutX(wheelR2.getLayoutX()-25);
+				}
+				
 			}
 		});
 	}
 	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean touch() {
-		boolean condition = false;
-		if (car.getLayoutX()< 360 && car.getLayoutX() > 10) {
-			condition = false;;
-		}else {
-			condition = true;
-		}
-		return condition;
-	}
 }
